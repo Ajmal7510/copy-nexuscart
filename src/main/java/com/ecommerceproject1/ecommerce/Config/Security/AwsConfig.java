29ee3c996922fc9f33201a35e5e24bf8a312a16b
Ajmal <116298673+Ajmal7510@users.noreply.github.com>
2024-02-09 11:07:18 +0530

    Create AwsConfig.java
    add aws config file

---
+++ src/main/java/com/ecommerceproject1/ecommerce/Config/Security/AwsConfig.java
@@ -0,0 +1,31 @@
+package com.ecommerceproject1.ecommerce.Config.Security;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsConfig {
+    @Value("${aws.accessKey}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
